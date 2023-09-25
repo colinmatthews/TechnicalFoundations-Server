c97efbf9ddcbcd1e9dfefbeb10678a25f3546633
Joe Manavalan (Medallia) <66131219+jpmanavalan@users.noreply.github.com>
2023-09-25 09:56:07 -0400

    Added line break
    Co-authored-by: colinmatthews <matthews.colin22@gmail.com>

--- index.js
+++ index.js
@@ -90,6 +90,7 @@ app.get('/recommend', (req, res) => {
       break;
     case 'keyboards':
       recommendation = 'Logitech MX Keys';
+      break;
     default:
       recommendation = 'Unknown product type';
   }
