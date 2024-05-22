563b1259af11b55cb3ac0f9dbdad15e6eeb320d3
Nick C. <11664418+ndrc81@users.noreply.github.com>
2024-05-22 01:54:31 +0000

    Commits for SDLC Assignment

--- api.test.js
+++ api.test.js
@@ -9,12 +9,25 @@ test('testing if jest works', () => {
 test('Testing root endpoint', async () => {
     const res = await request.get('/')
     const message = res.body.message
-    expect(message).toBe('hi!!')
+    expect(message).toBe('hi!')
 })
-
 
 test('Testing recommend endpoint', async () => {
     const res = await request.get('/recommend').query({'type':'fruits'})
     const message = res.body.recommendation
     expect(message).toBe('Apple')
 })
+
+test('Test fruit-check', async () => {
+    const res = await request.get('/fruit-check').query({'fruit':'Apple'})
+    const result = res.text
+    expect(result).toBe("Fruit already exists")
+})
+
+test('Testing recommend endpoint', async () => {
+    const res = await request.get('/recommend').query ({'type':'keyboards'})
+    
+        const message = res.body.recommendation
+        expect(message).toBe('Logitech MX Keys')
+})
+    
