0a25ff86c0c0c3c5c4ca3ec66568a882f21f7a3c
keith <think001.group@gmail.com>
2024-05-23 13:28:36 +1000

    Added new Keyboards endpoint

--- index.js
+++ index.js
@@ -98,6 +98,9 @@ app.get('/recommend', (req, res) => {
     case 'vegetables':
       recommendation = 'Carrot';
       break;
+    case 'keyboards':
+      recommendation = 'Logitech MX Keys';
+    break;
     default:
       recommendation = 'Unknown product type';
   }
