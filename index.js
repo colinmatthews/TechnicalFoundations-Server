a6ca10ef6bcf225ae2d43b0ecf770a77653202d7
celiagonzale <38985659+celiagonzale@users.noreply.github.com>
2024-06-28 07:47:58 +0000

    Add Kiwi -

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors())
 
 
 
-let fruits = ['Banana', 'Apple', 'Melon', 'Mangosteen', 'Peach', 'Raspberry', 'Blueberry', 'Avocado', 'Grapes','Pomegranite','Tangerine', 'Mango', 'Cherry', 'Kiwi' ]
+let fruits = ['Banana', 'Apple', 'Melon', 'Mangosteen', 'Peach', 'Raspberry', 'Blueberry', 'Avocado', 'Grapes','Pomegranite','Tangerine', 'Mango', 'Cherry', 'Tomato', 'Kiwi' ]
 
 
 
