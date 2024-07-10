d6fd45c7e70dca799b7651a0eb557a0f4ce4d85d
Tamana-R <trajwanicpa@gmail.com>
2024-07-10 02:36:09 +0000

    added fruit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors())
 
 
 
-let fruits = ['Banana', 'Apple', 'Melon', 'Mangosteen', 'Peach', 'Raspberry', 'Blueberry', 'Avocado', 'Grapes','Pomegranite','Tangerine', 'Mango', 'Cherry', 'Tomato' ]
+let fruits = ['Banana', 'Apple', 'Melon', 'Mangosteen', 'Peach', 'Raspberry', 'Blueberry', 'Avocado', 'Grapes','Pomegranite','Tangerine', 'Mango', 'Cherry', 'Tomato',"avocado" ]
 
 
 
