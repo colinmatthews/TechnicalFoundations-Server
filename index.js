45978a8d228261b00dfeb0398643bcf9f817159c
sarahmauer <sarah.mauer@jetblue.com>
2024-09-10 17:58:55 +0000

    Added 'Starfruit' and updated the spelling for 'Pomegranate'

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors())
 
 
 
-let fruits = ['Banana', 'Apple', 'Melon', 'Mangosteen', 'Peach', 'Raspberry', 'Blueberry', 'Avocado', 'Grapes','Pomegranite','Tangerine', 'Mango', 'Cherry', 'Tomato','Huckleberry' ]
+let fruits = ['Banana', 'Apple', 'Melon', 'Mangosteen', 'Peach', 'Raspberry', 'Blueberry', 'Avocado', 'Grapes','Pomegranate','Tangerine', 'Mango', 'Cherry', 'Tomato','Huckleberry','Starfruit' ]
 
 
 
