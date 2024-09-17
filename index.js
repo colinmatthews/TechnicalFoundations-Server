863af091dd34ed99effe319488681e7271849847
burchind <burcin@gmail.com>
2024-09-17 16:41:17 +0000

    added keyboard reco

--- index.js
+++ index.js
@@ -98,6 +98,11 @@ app.get('/recommend', (req, res) => {
     case 'fruits':
       recommendation = 'Apple';
       break;
+      
+    case 'keyboards':
+        recommendation = 'Logitech MX Keys';
+        break;
+  
     case 'vegetables':
       recommendation = 'Carrot';
       break;
