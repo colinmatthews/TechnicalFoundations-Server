f6339a76cc73ef71cc2da3756ff3cf329aa19842
rachelb543 <rachelhbanton@gmail.com>
2024-10-31 00:05:07 +0000

    adjusting hi!! to fix broken test

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ let fruits = ['Banana', 'Apple', 'Melon', 'Mangosteen', 'Peach', 'Raspberry', 'B
 
 
 app.get('/', async (req, res) => {
-  res.status(200).send({ message: 'hi!' })
+  res.status(200).send({ message: 'hi!!' })
 });
 
 app.get('/fruits', (req, res) => {
