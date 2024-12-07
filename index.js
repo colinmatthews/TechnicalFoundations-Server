9f5b44f0b1988846a0a8e7d0ad16073d41b53433
chal9000 <chris.halliday@hey.com>
2024-12-07 19:52:56 +0000

    Added keyboard recommendation

--- index.js
+++ index.js
@@ -88,6 +88,8 @@ app.get('/recommend', (req, res) => {
       break;
     case 'vegetables':
       recommendation = 'Carrot';
+    case 'keyboards':
+      recommendation = 'Logitech MX Keys';
       break;
     default:
       recommendation = 'Unknown product type';
