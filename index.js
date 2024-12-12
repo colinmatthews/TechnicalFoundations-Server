ce22c5e0a6de978d464e113b575b83ab4b609cb0
ziwei77 <ivanluzw@gmail.com>
2024-12-12 05:49:39 +0000

    Added Keyboard Recommendation

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ let fruits = ['Banana', 'Apple', 'Melon', 'Mangosteen', 'Peach', 'Raspberry', 'B
 
 
 app.get('/', async (req, res) => {
-  res.status(200).send({ message: 'hi!' })
+  res.status(200).send({ message: 'hi!!' })
 });
 
 app.get('/fruits', (req, res) => {
@@ -83,6 +83,9 @@ app.get('/recommend', (req, res) => {
   const type = req.query.type;
   let recommendation;
   switch(type) {
+    case 'keyboards':
+      recommendation = 'Logitech MX Keys';
+      break;
     case 'fruits':
       recommendation = 'Apple';
       break;
