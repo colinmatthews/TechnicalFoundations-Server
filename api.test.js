e0ebb0d18f8f3f9c7c88e52d6afa2a6ebd620933
mhhalle15 <marie-helene@nesto.ca>
2025-03-09 14:33:04 -0400

    Update api.test.js

--- api.test.js
+++ api.test.js
@@ -16,8 +16,9 @@ test('Testing root endpoint', async () => {
 test('Testing recommend endpoint', async () => {
     const res = await request.get('/recommend').query({'type':'fruits'})
     const message = res.body.recommendation
-    expect(message).toBe('Apple')
+    expect(message).toBe('Apple')  
 })
+
 
 test('Testing recommend endpoint', async () => {
 const res = await request.get('/recommend').query ({'type':'keyboards'})
