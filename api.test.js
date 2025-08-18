de84047999d7b96356b12f324cc2d80663f9b38a
SlowYourMind <35812121+SlowYourMind@users.noreply.github.com>
2025-08-18 07:27:16 +0000

    Added test for fruit-check

--- api.test.js
+++ api.test.js
@@ -13,8 +13,9 @@ test('Testing root endpoint', async () => {
 })
 
 
-test('Testing recommend endpoint', async () => {
-    const res = await request.get('/recommend').query({'type':'fruits'})
-    const message = res.body.recommendation
-    expect(message).toBe('Apple')
-})
+test('test fruit-check', async () => {
+  const res = await request.get('/fruit-check').query({fruit:"Apple"})
+  const result = res.text
+  expect(result).toBe("Fruit already exists")
+
+}
